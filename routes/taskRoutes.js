7a61aabe40d857228b939f5511e16f7cc14e0473
Mic <mic.cassano@gmail.com>
2016-02-26 10:00:29 +0000

    Refactor routes and rename Grunt/Nodemon scripts

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -2,15 +2,19 @@ var express = require('express');
 var router = express.Router();
 var Task = require('../models/taskModel');
 
-/* GET tasks listing. */
-router.get('/', function(req, res, next) {
-  Task.find(function (err, tasks) {
-    if (err) {
-      res.json({'ERROR': err});
-    } else {
-      res.json(tasks);
-    }
+// Router is mounter on '/tasks' path in app.js
+router.route('/')
+  .get(function(req, res, next) {
+    Task.find(function (err, tasks) {
+      if (err) {
+        res.json({'ERROR': err});
+      } else {
+        res.json(tasks);
+      }
+    });
+  })
+  .post(function (req, res, next) {
+
   });
-});
 
 module.exports = router;
